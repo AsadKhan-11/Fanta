3eeaffc3b69288784e532577691094049ab39406
Asad Khan <mrasad10khan@gmail.com>
2025-07-16 22:11:10 +0500

    Minor Adjustments

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import fanta1 from "../../assets/fanta1.png";
 import fanta2 from "../../assets/fanta2.png";
 import fanta3 from "../../assets/fanta3.png";
 import { motion, AnimatePresence, easeInOut } from "framer-motion";
 import Navbar from "../Navbar/Navbar";
 import { FaWhatsapp } from "react-icons/fa";
+
 const Drinks = [
   {
+    id: 1,
     title: "Fanta",
     subtitle:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias itaque soluta asperiores voluptate suscipit. Fugit libero corporis esse ipsa nemo necessitatibus repellendus deserunt eligendi expedita corrupti architecto autem laudantium nisi nihil odit qui ut praesentium ipsum, accusamus eaque est sint?",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias itaque soluta asperiores voluptate suscipit. ",
     price: "$40",
     modal: "Fanta",
     img: fanta1,
     bgColor: "#cf4f00",
   },
   {
+    id: 2,
     title: "Fanta Zero",
     subtitle:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias itaque soluta asperiores voluptate suscipit. Fugit libero corporis esse ipsa nemo necessitatibus repellendus deserunt eligendi expedita corrupti architecto autem laudantium nisi nihil odit qui ut praesentium ipsum, accusamus eaque est sint?",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias itaque soluta asperiores voluptate suscipit. Repellendus deserunt eligendi expedita corrupti",
     price: "$100",
     modal: "Zero",
     img: fanta2,
     bgColor: "#727272",
   },
   {
+    id: 3,
     title: "Coca Cola",
     subtitle:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias itaque soluta asperiores voluptate suscipit. Fugit libero corporis esse ipsa nemo necessitatibus repellendus deserunt eligendi expedita corrupti architecto autem laudantium nisi nihil odit qui ut praesentium ipsum, accusamus eaque est sint?",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias itaque soluta asperiores voluptate suscipit. ",
     price: "$10",
     modal: "Cola",
     img: fanta3,
@@ -56,8 +60,8 @@ const Hero = () => {
         },
       },
       exit: {
+        x: -50,
         opacity: 0,
-        x: -150,
         transition: {
           duration: 0.2,
           ease: easeInOut,
@@ -73,6 +77,8 @@ const Hero = () => {
     <>
       <motion.div
         initial={{ backgroundColor: activeData.bgColor }}
+        animate={{ backgroundColor: activeData.bgColor }}
+        transition={{ duration: 0.8 }}
         className=" "
       >
         {/* Navbar component */}
@@ -82,19 +88,22 @@ const Hero = () => {
         <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[500px]">
           <div className="flex flex-col justify-between py-14 md:py-0 xl:max-w-[500px] order-2 md:order-1 ">
             <div className="text-white space-y-5 text-center md:text-left">
-              <AnimatePresence node="wait">
+              <AnimatePresence mode="wait">
                 <motion.h1
+                  key={activeData.id}
+                  data-cursor-text=""
                   variants={slideRight(0.2)}
                   initial="hidden"
                   animate="visible"
                   exit="exit"
-                  className=" text-3xl lg:text-6xl xl:text-7xl leading-tight font-bold hand-writing text-shadow"
+                  className=" text-3xl lg:text-6xl xl:text-7xl leading-tight font-bold hand-writing text-shadow inline-block"
                 >
                   {activeData.title}
                 </motion.h1>
               </AnimatePresence>
-              <AnimatePresence node="wait">
+              <AnimatePresence mode="wait">
                 <motion.p
+                  key={activeData.id}
                   variants={slideRight(0.4)}
                   initial="hidden"
                   animate="visible"
@@ -104,8 +113,10 @@ const Hero = () => {
                   {activeData.subtitle}
                 </motion.p>
               </AnimatePresence>
-              <AnimatePresence node="wait">
+              <AnimatePresence mode="wait">
                 <motion.button
+                  key={activeData.id}
+                  style={{ color: activeData.bgColor }}
                   variants={slideRight(0.6)}
                   initial="hidden"
                   animate="visible"
@@ -119,6 +130,7 @@ const Hero = () => {
               {/* list separator */}
 
               <motion.div
+                key={activeData.id}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ ease: "easeInOut", duration: 0.5, delay: 0.2 }}
@@ -148,9 +160,7 @@ const Hero = () => {
                       />
                     </div>
                     <div className="text-center space-y-1 !mt-6">
-                      <p className="text-base line-through opacity-50">
-                        {data.modal}
-                      </p>
+                      <p className="text-base opacity-50">{data.modal}</p>
                       <p className="text-xl font-bold ">{data.price}</p>
                     </div>
                   </div>
@@ -160,15 +170,44 @@ const Hero = () => {
           </div>
           {/* Hero Image */}
           <div className="flex flex-col justify-end items-center order-1 md:order-2 relative">
-            <img
-              src={activeData.img}
-              alt={activeData.title}
-              className="w-[150px] md:w-[200px] xl:w-[300px] relative z-10"
-            />
-            <div className="text-[300px] font-extrabold font-poppins text-white/5 absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-              {activeData.modal}
-            </div>
+            <AnimatePresence mode="wait">
+              <motion.img
+                key={activeData.id}
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.4, delay: 0, ease: easeInOut }}
+                exit={{
+                  opacity: 0,
+                  x: -100,
+                  transition: {
+                    duration: 0.4,
+                  },
+                }}
+                src={activeData.img}
+                alt={activeData.title}
+                className="w-[150px] md:w-[200px] xl:w-[300px] relative z-10"
+              />
+            </AnimatePresence>
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={activeData.id}
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.4, delay: 0, ease: easeInOut }}
+                exit={{
+                  opacity: 0,
+                  x: -100,
+                  transition: {
+                    duration: 0.4,
+                  },
+                }}
+                className="text-[300px] font-extrabold font-poppins text-white/5 absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 "
+              >
+                {activeData.modal}
+              </motion.div>
+            </AnimatePresence>
           </div>
+
           <div className="text-3xl text-white fixed bottom-10 hover:rotate-[360deg] duration-500 z-[9999] mix-blend-difference">
             <a href="#">
               {" "}
